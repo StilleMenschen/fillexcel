3c54d46b0fb697f919512902ce4f973db84eecff
Stillemenschen <2956686701@qq.com>
2023-07-03 23:26:16 +0800

    feat: 分页查询生成规则和参数

--- fills/views.py
+++ fills/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.views import generic
 from django.views.decorators.http import require_GET, require_http_methods
 
-from .models import FillingRequirement
+from .models import FillingRequirement, GenerateRule, GenerateRuleParameter
 from .service import fill_excel
 
 
@@ -27,17 +27,11 @@ def generates(request, req_id: int):
     return JsonResponse(dict(requirement=req_id, took=took))
 
 
-@require_GET
-def get_paging_requirement_list(request):
-    all_fr = FillingRequirement.objects.all().values()
-
-    size = request.GET.get('size', default=8)
-    paginator = Paginator(all_fr, per_page=size)
-    # 获取请求参数中的页码
-    page_number = request.GET.get('page', default=1)
+def base_list(obj, page: int = 1, size: int = 8):
+    paginator = Paginator(obj, per_page=size)
 
     # 获取当前页的记录对象
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator.get_page(page)
 
     # 构建返回的JSON数据
     response_data = {
@@ -53,3 +47,30 @@ def get_paging_requirement_list(request):
 
     # 返回JSON响应
     return JsonResponse(response_data)
+
+
+@require_GET
+def get_requirement_list(request):
+    all_fr = FillingRequirement.objects.order_by('-id').values()
+
+    page = request.GET.get('page', default=1)
+    size = request.GET.get('size', default=8)
+    return base_list(all_fr, page, size)
+
+
+@require_GET
+def get_generate_rule_list(request):
+    all_gr = GenerateRule.objects.order_by('id').values()
+
+    page = request.GET.get('page', default=1)
+    size = request.GET.get('size', default=8)
+    return base_list(all_gr, page, size)
+
+
+@require_GET
+def get_generate_rule_parameter_list(request):
+    all_gr = GenerateRuleParameter.objects.order_by('rule_id', 'id').values()
+
+    page = request.GET.get('page', default=1)
+    size = request.GET.get('size', default=8)
+    return base_list(all_gr, page, size)
